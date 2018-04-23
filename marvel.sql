34dc263cccf40126e279a5ed0c7cccc138fda2d7
rogersonpi <iain.rogerson77@googlemail.com>
2018-04-23 18:02:05 +0100

    final question

--- marvel.sql
+++ marvel.sql
@@ -97,14 +97,23 @@ INSERT INTO movies (title, year, show_time) VALUES ('Doctor Strange', 2016, '17:
 -- DELETE FROM movies WHERE title = 'Batman Begins';
 -- SELECT * FROM movies;
 -- 6. Create a new entry in the 'people' table with the name of one of the instructors.
-INSERT INTO people(name)
-VALUES ('Sandy');
-SELECT * FROM people;
+-- INSERT INTO people(name)
+-- VALUES ('Sandy');
+-- SELECT * FROM people;
 -- 7. Zsolt, has decided to hijack our movie evening, Remove him from the table of people.
+-- DELETE FROM people WHERE name = 'Zsolt Podoba-Szalai';
+-- SELECT * FROM people;
 -- 8. Somehow the list of people includes two people named 'Andrew'. Change these entries to the proper names ('Jeff 4', 'Jeff 5')
+-- UPDATE people SET name = 'Jeff 4' WHERE name = 'Andrew Smith';
+-- UPDATE people SET name = 'Jeff 5' WHERE name = 'Andrew Laughlin';
+-- SELECT * FROM people;
 -- 9. The cinema has just heard that they will be holding an exclusive midnight showing of 'Guardians of the Galaxy 2'!! Create a new entry in the 'movies' table to reflect this.
+-- INSERT INTO movies(title)
+-- VALUES ('Guardians of the Galaxy 2');
+-- SELECT * FROM movies;
 -- 10. The cinema would also like to make the Guardian movies a back to back feature. Update the 'Guardians of the Galaxy' show time from 18:55 to 21:30
---
+-- UPDATE movies SET show_time = '21:30' WHERE title = 'Guardians of the Galaxy';
+-- SELECT * FROM movies;
 -- ## Extension
 --
 -- 1. Research how to delete multiple entries from your table in a single command.
